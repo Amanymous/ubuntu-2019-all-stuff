e692cbc7c0dc0d9db6a96755f40c16eb2ec873fa
aman mirza <aman mirza99>
2020-04-17 14:48:08 +0500

    Updates

---
+++ precache-manifest.d38f16f19fdf075da39563a0c2b8c61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e7edf6e76598fdcfc55cc5f474fe75",
+    "url": "/website-React/index.html"
+  },
+  {
+    "revision": "6cd43e236f9219a65423",
+    "url": "/website-React/static/css/main.5c8c1a7d.chunk.css"
+  },
+  {
+    "revision": "62a47b0474832aeb7293",
+    "url": "/website-React/static/js/2.0f5c875b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website-React/static/js/2.0f5c875b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd43e236f9219a65423",
+    "url": "/website-React/static/js/main.91b9dc14.chunk.js"
+  },
+  {
+    "revision": "b9c1b4da0a03596d0f53",
+    "url": "/website-React/static/js/runtime-main.4fecb004.js"
+  }
+]);
